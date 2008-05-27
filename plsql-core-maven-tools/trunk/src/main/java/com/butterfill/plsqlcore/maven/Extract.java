237fa7c671482221ab9b97a8dc585a5876ed9413
Peter Butterfill <pete88b@users.noreply.github.com>
2008-05-27 19:03:04 +0000

    minor changes

--- plsql-core-maven-tools/trunk/src/main/java/com/butterfill/plsqlcore/maven/Extract.java
+++ plsql-core-maven-tools/trunk/src/main/java/com/butterfill/plsqlcore/maven/Extract.java
@@ -6,28 +6,23 @@ import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.project.MavenProject;
 
 /**
- *
+ * Provides access to the extract functionality of plsql-core.
+ * 
  * @goal Extract
  */
 public class Extract extends AbstractMojo {
     
     /**
-     * This populates the maven project.
-     * 
-     * @parameter expression="${project}"
-     */
-    private MavenProject project;
-    
-    /**
+     * The plsql-core includes file which can be null.
      * 
      * @parameter expression="${includesFile}"
      */
     private File includesFile;
     
     /**
+     * The name of the output directory.
      * 
      * @parameter expression="${outputDirectoryName}"
      */
@@ -39,6 +34,17 @@ public class Extract extends AbstractMojo {
     public Extract() {
     }
 
+    /**
+     * Calls ExtractorMain#extract having set the output directory name, 
+     * passing in the includes file if it has been provided.
+     * 
+     * @see #setIncludesFile(java.io.File)
+     * @see #setOutputDirectoryName(java.lang.String)
+     * @throws org.apache.maven.plugin.MojoExecutionException
+     *   If the extract fails.
+     * @throws org.apache.maven.plugin.MojoFailureException
+     *   If the extract fails.
+     */
     public void execute() throws MojoExecutionException, MojoFailureException {
         ExtractorMain app = new ExtractorMain();
         
@@ -60,28 +66,42 @@ public class Extract extends AbstractMojo {
         
     }
 
-    public MavenProject getProject() {
-        return project;
-    }
-
-    public void setProject(MavenProject project) {
-        this.project = project;
-    }
-
+    /**
+     * Returns the includes file to use when extracting.
+     * 
+     * @return The includes file to use when extracting.
+     */
     public File getIncludesFile() {
         return includesFile;
     }
 
-    public void setIncludesFile(File includesFile) {
-        this.includesFile = includesFile;
+    /**
+     * Sets the includes file to use when extracting.
+     * 
+     * @param file
+     *   The includes file to use when extracting.
+     */     
+    public void setIncludesFile(final File file) {
+        this.includesFile = file;
     }
 
-    public String getBaseDirName() {
+    /**
+     * Returns the output directory name to use when extracting.
+     * 
+     * @return The output directory name to use when extracting.
+     */
+    public String getOutputDirectoryName() {
         return outputDirectoryName;
     }
 
-    public void setBaseDirName(String baseDirName) {
-        this.outputDirectoryName = baseDirName;
+    /**
+     * Sets the output directory name to use when extracting.
+     * 
+     * @param name
+     *   The output directory name to use when extracting.
+     */
+    public void setOutputDirectoryName(final String name) {
+        this.outputDirectoryName = name;
     }
 
 } // End of class Extract
--- plsql-core-maven-tools/trunk/src/test/java/com/butterfill/plsqlcore/maven/ExtractTest.java
+++ plsql-core-maven-tools/trunk/src/test/java/com/butterfill/plsqlcore/maven/ExtractTest.java
@@ -5,9 +5,11 @@
 
 package com.butterfill.plsqlcore.maven;
 
+import java.io.File;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.maven.plugin.MojoExecutionException;
 
 /**
  *
@@ -40,8 +42,62 @@ public class ExtractTest extends TestCase {
     public void testExecute() throws Exception {
         System.out.println("execute");
         Extract instance = new Extract();
-        instance.setBaseDirName("C:/TMP/temp/");
+        String temp = System.getProperty("java.io.tmpdir");
+        System.out.println("setting output dir to " + temp);
+        instance.setOutputDirectoryName(temp);
+        instance.setIncludesFile(new File("target/test-classes/plsq-core-includes-test.txt"));
         instance.execute();
+        instance.setIncludesFile(null);
+        instance.execute();
+        instance.setIncludesFile(new File("does-not-exist"));
+        try {
+            instance.execute();
+            fail();
+        } catch (MojoExecutionException ex) {
+        }
+        
+    }
+
+    /**
+     * Test of getIncludesFile method, of class Extract.
+     */
+    public void testGetIncludesFile() {
+        System.out.println("getIncludesFile");
+        Extract instance = new Extract();
+        File expResult = null;
+        File result = instance.getIncludesFile();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setIncludesFile method, of class Extract.
+     */
+    public void testSetIncludesFile() {
+        System.out.println("setIncludesFile");
+        File file = null;
+        Extract instance = new Extract();
+        instance.setIncludesFile(file);
+    }
+
+    /**
+     * Test of getOutputDirectoryName method, of class Extract.
+     */
+    public void testGetOutputDirectoryName() {
+        System.out.println("getOutputDirectoryName");
+        Extract instance = new Extract();
+        String expResult = null;
+        String result = instance.getOutputDirectoryName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setOutputDirectoryName method, of class Extract.
+     */
+    public void testSetOutputDirectoryName() {
+        System.out.println("setOutputDirectoryName");
+        String name = "";
+        Extract instance = new Extract();
+        instance.setOutputDirectoryName(name);
     }
 
 }
