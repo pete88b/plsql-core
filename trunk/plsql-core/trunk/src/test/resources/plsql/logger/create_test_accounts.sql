0c5234cbf8a8b9b63fe9e4a820ce4005dd195605
peter.butterfill <peter.butterfill@0e6f96ae-d64d-0410-bb67-01c4cf5365fb>
2008-05-28 15:35:37 +0000

    added licence headers

--- trunk/plsql-core/trunk/src/test/resources/plsql/logger/create_test_accounts.sql
+++ trunk/plsql-core/trunk/src/test/resources/plsql/logger/create_test_accounts.sql
@@ -1,34 +1,23 @@
+/**
+ * Copyright (C) 2008 Peter Butterfill.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 /*
   Creates accounts needed by logger test scripts.
 */
--- c3adev run as DBA
-sqlplus /
-
-drop user logger_test cascade;
- 
-create user logger_test
-  identified by star01
-  default tablespace CT_USERS
-  temporary tablespace USERTEMP
-  profile DEFAULT;
-
-grant dba to logger_test;
-grant execute any procedure to logger_test;
-grant select any table to logger_test;
-
-
-drop user logger_test2 cascade;
- 
-create user logger_test2
-  identified by star01
-  default tablespace CT_USERS
-  temporary tablespace USERTEMP
-  profile DEFAULT;
-
-grant dba to logger_test2;
-grant execute any procedure to logger_test2;
-
-
 
 -- XE run as SYS
 drop user logger_test cascade;
