1d6355f7dbf0d356fa867f26f0a506a4a412a6fe
Peter Butterfill <pete88b@users.noreply.github.com>
2008-05-28 15:35:37 +0000

    added licence headers

--- plsql-core/trunk/src/test/resources/plsql/logger_pipe/create_test_account.sql
+++ plsql-core/trunk/src/test/resources/plsql/logger_pipe/create_test_account.sql
@@ -1,22 +1,18 @@
--- c3adev run as DBA
-sqlplus /
-
-drop user logger_test cascade;
- 
-create user logger_test
-  identified by star01
-  default tablespace CT_USERS
-  temporary tablespace USERTEMP
-  profile DEFAULT;
-
-grant dba to logger_test;
-
-grant execute any procedure to logger_test;
-
-grant select any table to logger_test;
-
-
-
+/**
+ * Copyright (C) 2008 Peter Butterfill.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
 -- XE run as SYS
