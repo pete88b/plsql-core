3ed1ab48173588dce5a26ac6b40e990070a4b180
Peter Butterfill <pete88b@users.noreply.github.com>
2008-07-29 13:49:27 +0000

    added execute and executeIfNoNetwork options

--- plsql-core-maven-tools/trunk/src/test/java/com/butterfill/plsqlcore/maven/ExtractTest.java
+++ plsql-core-maven-tools/trunk/src/test/java/com/butterfill/plsqlcore/maven/ExtractTest.java
@@ -6,6 +6,8 @@
 package com.butterfill.plsqlcore.maven;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLConnection;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -17,8 +19,61 @@ import org.apache.maven.plugin.MojoExecutionException;
  */
 public class ExtractTest extends TestCase {
     
+    private boolean networkOk;
+    
+    final String EXTRACT_DIR_NAME = 
+            System.getProperty("java.io.tmpdir") +
+            File.separator + "ExtractTest";
+    
+    private void deleteDirectory(File dir) {
+        if (!dir.isDirectory()) {
+            return;
+        }
+        
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteDirectory(file);
+
+                } else {
+                    if (!file.delete()) {
+                        throw new RuntimeException("failed to delete: " + file.getAbsolutePath());
+                    }
+
+                }
+
+            }
+            
+        } // End of if (files != null)
+        
+        if (!dir.delete()) {
+            throw new RuntimeException("failed to delete: " + dir.getAbsolutePath());
+            
+        } else {
+            System.out.println("Deleted: " + dir.getAbsolutePath());
+            
+        }
+        
+    }
+
+    
     public ExtractTest(String testName) {
         super(testName);
+        
+        try {
+            URLConnection urlConnection = new URL("http://www.sun.com").openConnection();
+            urlConnection.setUseCaches(false);
+            urlConnection.getInputStream();
+            networkOk = true;
+            
+        } catch (Exception ex) {
+            networkOk = false;
+
+        }
+        
+        deleteDirectory(new File(EXTRACT_DIR_NAME));
+        
     }
 
     public static Test suite() {
@@ -42,11 +97,9 @@ public class ExtractTest extends TestCase {
     public void testExecute() throws Exception {
         System.out.println("execute");
         Extract instance = new Extract();
-        String temp = System.getProperty("java.io.tmpdir");
-        temp += File.separator;
-        temp += "ExtractTest";
-        System.out.println("setting output dir to " + temp);
-        instance.setOutputDirectory(temp);
+        
+        System.out.println("setting output dir to " + EXTRACT_DIR_NAME);
+        instance.setOutputDirectory(EXTRACT_DIR_NAME);
         instance.setIncludesFile(new File("target/test-classes/plsq-core-includes-test.xml"));
         instance.execute();
         instance.setIncludesFile(new File("target/test-classes/plsq-core-includes-test-empty.xml"));
@@ -54,14 +107,41 @@ public class ExtractTest extends TestCase {
         instance.setIncludesFile(null);
         try {
             instance.execute();
-            fail();
+            if (networkOk) {
+                fail();
+            }
         } catch (MojoExecutionException ex) {
+            if (!networkOk) {
+                fail();
+            }
         }
         instance.setIncludesFile(new File("does-not-exist"));
         try {
             instance.execute();
-            fail();
+            if (networkOk) {
+                fail();
+            }
         } catch (MojoExecutionException ex) {
+            if (!networkOk) {
+                fail();
+            }
+        }
+        
+        instance.setExecute(false);
+        instance.execute();
+        
+        instance.setExecute(true);
+        instance.setIncludesFile(new File("target/test-classes/plsq-core-includes-test.xml"));
+        instance.setExecuteIfNoNetwork(true);
+        try {
+            instance.execute();
+            if (!networkOk) {
+                fail();
+            }
+        } catch (MojoExecutionException ex) {
+            if (networkOk) {
+                fail();
+            }
         }
     }
 
@@ -119,5 +199,14 @@ public class ExtractTest extends TestCase {
         assertEquals("a" + File.separator, instance.getOutputDirectory());
     }
 
-
+    public void testIsExecute() throws Exception {
+        System.out.println("isExecute()");
+        assertTrue(new Extract().isExecute());
+    }
+    
+    public void testIsExecuteIfNoNetwork() throws Exception {
+        System.out.println("isExecuteIfNoNetwork()");
+        assertFalse(new Extract().isExecuteIfNoNetwork());
+    }
+    
 }
