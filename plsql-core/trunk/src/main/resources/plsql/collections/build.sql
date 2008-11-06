a70c6e98f99a77d00b961718545dbeae1850f334
Peter Butterfill <pete88b@users.noreply.github.com>
2008-11-06 18:13:55 +0000

    minor change

--- plsql-core/trunk/src/main/resources/plsql/collections/build.sql
+++ plsql-core/trunk/src/main/resources/plsql/collections/build.sql
@@ -23,7 +23,7 @@ CREATE OR REPLACE TYPE varchar_table IS TABLE OF VARCHAR2(4000);
 CREATE OR REPLACE TYPE number_table IS TABLE OF NUMBER;
 /
 
-PROMPT Creating collections specification
+PROMPT Creating collections package
 @@collections.pck
 
 PROMPT ___ End of collections build.sql ___
